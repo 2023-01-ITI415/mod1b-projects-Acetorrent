7cc4a56b69b1abdfc458af6a43f40e3818a0d20c
Acetorrent <phihoangkhai2811@gmail.com>
2023-02-02 20:14:10 -0500

    Finalizing the game WebGL and complete Score

--- Assets/supportScripts/LoadNewScene.cs
+++ Assets/supportScripts/LoadNewScene.cs
@@ -8,7 +8,7 @@ public class LoadNewScene : MonoBehaviour
 
     public void LoadScene1()
     {
-        SceneManager.LoadScene("Main-ApplePicker");
+        SceneManager.LoadScene("Main-Apple Picker");
     }
 
     public void LoadScene2()
