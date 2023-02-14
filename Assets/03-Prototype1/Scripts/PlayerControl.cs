8f41c5a8ed01e140efe778a56527525993754e1b
Acetorrent <phihoangkhai2811@gmail.com>
2023-02-13 21:07:14 -0500

    Messing with stats

---
+++ Assets/03-Prototype1/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+using UnityEngine.UIElements;
+
+public class PlayerControl : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public float speed = 5;
+    Rigidbody rb;
+    Vector3 position = new Vector3(0f, 0f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector3.Lerp(transform.position, mousePosition, speed);
+
+    }
+}
